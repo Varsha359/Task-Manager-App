b7e864177e120fece9fffe4370e441ef6e621615
G Varsha <66876505+Varsha359@users.noreply.github.com>
2025-11-15 01:02:29 +0530

    added dependencies and back4app config

---
+++ task_manager_app/lib/utils/constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String appId = "VaFsMlnQdicmhdKLgIuC8KtP0cxVZEq0q9B3fcbb";
+  static const String clientKey = "Mck7IrjRADe8HXrlKFvfy3ZTH01olBVW5M1jrLYi";
+  static const String serverUrl = "https://parseapi.back4app.com";
+}
